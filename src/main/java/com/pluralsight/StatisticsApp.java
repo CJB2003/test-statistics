215287015faf59799e8431f4326dc9dc5a797383
chrisbunny <christopherjbunny@gmail.com>
2026-04-17 21:11:07 -0500

    Revamped entire array system. Instead of hard coding array, I've initialized it as an empty array with a fixed length of 10, using a for loop to generate random numbers between 70 and 100 storing them into each index of the array. Also finished a for loop that calculates the total and average of the test scores.

--- src/main/java/com/pluralsight/StatisticsApp.java
+++ src/main/java/com/pluralsight/StatisticsApp.java
@@ -1,21 +1,36 @@
 package com.pluralsight;
+import java.util.*;
 import java.util.Arrays;
 
 public class StatisticsApp {
     public static void main(String[] args) {
 
-        //created array variable testScores and store the array method getScores() in there
-        int[] testScores = getScores();
+        //created array variable testScores with a fixed size of 10 but no numbers yet
+        int[] testScores = new int[10];
+
+        //Iterates the length of the empty array testScores and stores a random value into every index
+        for (int i = 0; i < testScores.length; i++) {
+
+            //Kept getting the numbers above 100, realized you have to multiply by max - min + 1
+            //which is how I got 31 and add 70 for the min
+            int randomScores = (int)(Math.random() * 31) + 70;
+            testScores[i] = randomScores;
+        }
+        //prints the array in array format
+        System.out.println(Arrays.toString(testScores));
+
+        //Initialized variables with default values to calculate total score and average score
+        double sumScores = 0;
+        double avgScores = 0;
 
         //using for loop to iterate through the array and printing out all the values
-        for (int i = 0; i < testScores.length; i++) {
-            System.out.println(testScores[i] + " ");
+        for (int testScore : testScores) {
+            System.out.println(testScore);
+            sumScores += testScore;
+            avgScores = sumScores / 10;
         }
-        System.out.println();
+        System.out.println("Total: " + sumScores);
+        System.out.println("The average score of the class is: " + avgScores);
     }
-    //array method storing 10 test score values
-    public static int[] getScores() {
-        int[] testNums = {90, 85, 82, 80, 95, 99, 76, 50, 65, 75};
-        return testNums;
-    }
+
 }
